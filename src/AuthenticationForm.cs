4c34d07a2e581c53248bc7f83d2b45e2fa7983b5
Htoo Pyae Linn <85918337+htoopyaelinn56@users.noreply.github.com>
2025-11-04 13:09:40 +0630

    Login/Register validation

--- src/AuthenticationForm.cs
+++ src/AuthenticationForm.cs
@@ -33,7 +33,7 @@ namespace FreelancePlatform
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-          
+            var validationPass = CheckValidation(false);
         }
 
         private String? GetUserType()
@@ -45,23 +45,59 @@ namespace FreelancePlatform
             return clientRadioButton.Checked ? "client" : "freelancer";
         }
 
-        private bool IsPasswordValidForRegister()
+        private bool IsPasswordValid(bool isRegister)
         {
             var password = passwordTextBox.Text;
 
             if (string.IsNullOrWhiteSpace(password))
             {
-                MessageBox.Show("Password fields cannot be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Password cannot be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
             }
 
-            if (password.Length < 8)
+            if (password.Length < 8 && isRegister)
             {
                 MessageBox.Show("Password must be at least 8 characters long.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return false;
             }
 
             return true;
+        }
+
+        private bool CheckValidation(bool isRegister)
+        {
+            var userType = GetUserType();
+            var userName = userNameTextBox.Text;
+
+
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                MessageBox.Show("Username cannot be empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            var passwordValid = IsPasswordValid(isRegister);
+            if (!passwordValid)
+            {
+                return false;
+            }
+
+            else if (userType == null)
+            {
+                MessageBox.Show("Please select User Type!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
+
+        }
+
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            var validationPass = CheckValidation(true);
         }
     }
 }
