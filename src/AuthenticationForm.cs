f1d5135ccc61e4db3ac241197da57262b4f53ed5
Htoo Pyae Linn <85918337+htoopyaelinn56@users.noreply.github.com>
2025-11-04 10:21:13 +0630

    Authentication Form UI

--- src/AuthenticationForm.cs
+++ src/AuthenticationForm.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace FreelancePlatform
 {
     public partial class AuthenticationForm : BaseForm
     {
+        bool showPassword = false;
         public AuthenticationForm()
         {
             InitializeComponent();
@@ -23,9 +25,43 @@ namespace FreelancePlatform
 
         }
 
-        private void formTitle_Click(object sender, EventArgs e)
+        private void showHidePasswordCheckbox_CheckedChanged(object sender, EventArgs e)
         {
+            showPassword = !showPassword;
+            passwordTextBox.UseSystemPasswordChar = !showPassword;
+        }
 
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private String? GetUserType()
+        {
+            if (!clientRadioButton.Checked && !freelancerRadioButton.Checked)
+            {
+                return null;
+            }
+            return clientRadioButton.Checked ? "client" : "freelancer";
+        }
+
+        private bool IsPasswordValidForRegister()
+        {
+            var password = passwordTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Password fields cannot be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (password.Length < 8)
+            {
+                MessageBox.Show("Password must be at least 8 characters long.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            return true;
         }
     }
 }
