1a5d9fe3591d4a06f506b4e277482d5b3c15679f
Htoo Pyae Linn <85918337+htoopyaelinn56@users.noreply.github.com>
2025-11-07 20:56:22 +0630

    Client Profile Data

--- src/ClientProfileForm.cs
+++ src/ClientProfileForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,7 +22,7 @@ namespace FreelancePlatform.src
 
         private void ClientProfileForm_Load(object sender, EventArgs e)
         {
-
+            GetClientProfileData();
         }
 
         private void editProfileButton_Click(object sender, EventArgs e)
@@ -59,5 +60,44 @@ namespace FreelancePlatform.src
             var authenticationForm = new AuthenticationForm();
             authenticationForm.Show();
         }
+
+        private void GetClientProfileData()
+        {
+            var conn = DatabaseService.GetConnection();
+
+            string query = @"
+                SELECT u.id, u.username, u.email, u.phone,
+                       c.address, c.company_name
+                FROM Users u
+                INNER JOIN Clients c ON u.id = c.user_id
+                WHERE u.id = @userId;";
+
+            using (var cmd = new MySqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@userId", this.userId);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        int id = reader.GetInt32("id");
+                        string username = reader.GetString("username");
+                        string email = reader.IsDBNull(reader.GetOrdinal("email")) ? "" : reader.GetString("email");
+                        string phone = reader.IsDBNull(reader.GetOrdinal("phone")) ? "" : reader.GetString("phone");                     
+
+                        string address = reader.IsDBNull(reader.GetOrdinal("address")) ? "" : reader.GetString("address");
+                        string companyName = reader.IsDBNull(reader.GetOrdinal("company_name")) ? "" : reader.GetString("company_name");
+
+                        nameValue.Text = username;
+                        emailValue.Text = email;
+                        phoneValue.Text = phone;
+                        companyNameValue.Text = companyName;
+                        addressValue.Text = address;
+
+                    }
+                }
+            }
+        }
+
     }
 }
